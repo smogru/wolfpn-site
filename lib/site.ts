48306a9b6f5801eece539247305c40846c827ff7
smogru <smogrush@gmail.com>
2026-08-14 20:52:40 +0300

    Delete lib directory

--- lib/site.ts
+++
@@ -1,9 +0,0 @@
-export const SITE_URL = (
-  process.env.NEXT_PUBLIC_SITE_URL ||
-  "https://wolfpn.was-biting2v2anhzj.chatgpt.site"
-).replace(/\/$/, "");
-
-export const BOT_URL = "https://t.me/WolfPN_bot?start=ref_REF-3CF3E36D";
-export const REGISTRATION_URL = "https://wolfpn-v2.com/cabinet/register?ref=REF-3CF3E36D";
-export const SUPPORT_URL = "https://t.me/WolfPNsupport_bot";
-
